35d2cc592535036c678e5972b448b9de8e232da3
Yann <yann.legendre@essec.edu>
2019-08-19 17:49:19 +0200

    new : route, controller et vue

--- app/controllers/skis_controller.rb
+++ app/controllers/skis_controller.rb
@@ -4,12 +4,17 @@ class SkisController < ApplicationController
   end
 
   def index
+    #before we create the actual form, we test the filtering thing "a la mano"
+    @skis = Ski.where(city: 'Lyon')
   end
 
   def show
   end
 
   def create
+  end
+
+  def new
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :skis, only: [:index, :create, :show]
+  resources :skis, only: [:index, :create, :show, :new]
 
   root to: 'skis#search'
 
