5fcced87e69e74fcf661d9ff7de44abbf993182d
Yann <yann.legendre@essec.edu>
2019-08-22 13:38:00 +0200

    maj

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,15 +5,21 @@ import 'flatpickr/dist/flatpickr.min.css'
 import { loadDynamicBannerText } from '../components/banner';
 
 Rails.start();
-loadDynamicBannerText();
 
-const calendar = document.querySelector('#calendar')
-const datesBooked = calendar.dataset.datesBooked
-flatpickr("#transaction_rental_date",
-{
-  minDate: "today",
-  disable: JSON.parse(datesBooked),
-  dateFormat: "d-m-Y"
-});
+const banner = document.querySelector('#banner-typed-text');
+if (banner) {
+  loadDynamicBannerText();
+}
 
+
+const calendar = document.querySelector('#calendar');
+if (calendar) {
+  const datesBooked = calendar.dataset.datesBooked
+  flatpickr("#transaction_rental_date",
+  {
+    minDate: "today",
+    disable: JSON.parse(datesBooked),
+    dateFormat: "d-m-Y"
+  });
+}
 
