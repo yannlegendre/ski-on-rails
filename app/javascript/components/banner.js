2331240c76cd017a49c06826981e67694a5c6a0a
Jean Luc <gaman2404@gmail.com>
2019-08-22 11:01:37 +0200

    page search ok

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["Skis", "Snows", "Sleds"],
+    strings: ["Skis", "Snows", "Sledges"],
     typeSpeed: 120,
     loop: true
   });
